b7ff67015d25b549a8a7dd7300604dbbe86c1a95
Alexander Keliris <rigellute@gmail.com>
2020-01-27 16:43:43 +0000

    Remove "user can post once a day "requirement

--- steps/src/main.rs
+++ steps/src/main.rs
@@ -3,10 +3,9 @@ use chrono::prelude::*;
 use core::{CustomOutput, Steps};
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use rusoto_core::Region;
-use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, PutItemInput, QueryInput};
+use rusoto_dynamodb::{DynamoDb, DynamoDbClient, PutItemInput};
 use serde::Deserialize;
 use simple_error::bail;
-use std::collections::HashMap;
 use std::env;
 use uuid::Uuid;
 
@@ -25,69 +24,11 @@ fn handler(event: ApiGatewayProxyRequest, context: Context) -> Result<CustomOutp
 
     let utc: DateTime<Utc> = Utc::now();
     let now_as_millis = utc.timestamp_millis();
-    let num_millis_from_midnight = utc.num_seconds_from_midnight() * 1000;
     let user_id = event
         .request_context
         .identity
         .cognito_identity_id
         .expect("Not authorized: no user_id present.");
-
-    /*
-     * First, validate that the user has not added any steps within the last day (i.e. since midnight)
-     * Users should only be able to add their steps once a day.
-     */
-    // The property "timestamp" is a reserved word in DynamoDb, so we must rename it
-    let mut expression_attribute_names = HashMap::new();
-    expression_attribute_names.insert("#ts".to_string(), "timestamp".to_string());
-
-    let mut expression_attribute_values = HashMap::new();
-    expression_attribute_values.insert(
-        ":user_id".to_string(),
-        AttributeValue {
-            s: Some(user_id.clone()),
-            ..Default::default()
-        },
-    );
-
-    expression_attribute_values.insert(
-        ":midnight".to_string(),
-        AttributeValue {
-            n: Some((now_as_millis - num_millis_from_midnight as i64).to_string()),
-            ..Default::default()
-        },
-    );
-
-    let query = QueryInput {
-        table_name: table_name.clone(),
-        key_condition_expression: Some("user_id = :user_id".to_string()),
-        filter_expression: Some("#ts > :midnight".to_string()),
-        expression_attribute_values: Some(expression_attribute_values),
-        expression_attribute_names: Some(expression_attribute_names),
-        ..QueryInput::default()
-    };
-
-    let has_added_steps_since_midnight = match client.query(query).sync() {
-        Ok(res) => match res.items {
-            Some(items) => !items.is_empty(),
-            None => false,
-        },
-        Err(e) => {
-            println!(
-                "Error checking if user has posted in last day: id - {}, error - {}",
-                context.aws_request_id, e
-            );
-            bail!("Something went wrong");
-        }
-    };
-
-    if has_added_steps_since_midnight {
-        let not_allowed = CustomOutput {
-            status_code: 400,
-            ..Default::default()
-        };
-
-        return Ok(not_allowed);
-    }
 
     let body = event
         .body
